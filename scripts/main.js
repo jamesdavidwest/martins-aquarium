8917003cb4949d3ea221cb9ff9e5a704cd8ff732
James David West <thefakejddubs@gmail.com>
2024-02-29 16:29:33 -0600

    updated html and css, as well as added a bit more to database

--- scripts/main.js
+++ scripts/main.js
@@ -40,16 +40,35 @@ const quoteList = {
 			author: "Mermaid Melody",
 		},
 	],
-	scubaDivingTips: [
-		"Stay calm and conserve your air. Control your breathing and move slowly to maximize your dive time and enjoy the underwater scenery.",
-		"Maintain proper buoyancy to protect delicate marine life and avoid damaging coral reefs. Practice good buoyancy control to minimize your impact on the environment.",
-		"Always plan your dive and dive your plan. Make sure you're familiar with the dive site, check the weather and currents, and communicate with your dive buddy to ensure a safe and enjoyable experience.",
-	],
-	fishHarvestingTips: [
-		"Handle fish with care to ensure their freshness and quality. Use a soft landing net or wet hands to avoid damaging their delicate scales and flesh.",
-		"Know your local regulations and practice responsible fishing. Respect catch limits, release undersized or protected species, and use sustainable fishing methods to preserve marine ecosystems.",
-		"Keep your fishing gear clean and well-maintained for optimal performance. Regularly check your hooks, lines, and reels to prevent equipment failure and ensure successful catches.",
-	],
+};
+
+const scubaDivingTips = [
+	"Stay calm and conserve your air. Control your breathing and move slowly to maximize your dive time and enjoy the underwater scenery.",
+	"Maintain proper buoyancy to protect delicate marine life and avoid damaging coral reefs. Practice good buoyancy control to minimize your impact on the environment.",
+	"Always plan your dive and dive your plan. Make sure you're familiar with the dive site, check the weather and currents, and communicate with your dive buddy to ensure a safe and enjoyable experience.",
+];
+
+const fishHarvestingTips = [
+	"Handle fish with care to ensure their freshness and quality. Use a soft landing net or wet hands to avoid damaging their delicate scales and flesh.",
+	"Know your local regulations and practice responsible fishing. Respect catch limits, release undersized or protected species, and use sustainable fishing methods to preserve marine ecosystems.",
+	"Keep your fishing gear clean and well-maintained for optimal performance. Regularly check your hooks, lines, and reels to prevent equipment failure and ensure successful catches.",
+];
+
+const tankCleaningTips = [
+	{
+		"Regular Water Changes":
+			"Perform regular water changes to remove waste and maintain water quality. Aim to replace about 10-20% of the tank's water every 1-2 weeks, depending on the size of the tank and the number of fish.",
+		"Vacuum Gravel":
+			"Use a gravel vacuum to clean the substrate in the tank. This will remove debris and waste that has settled on the bottom of the tank, helping to prevent the buildup of harmful substances and maintain a clean environment for your fish.",
+		"Clean Decorations and Equipment":
+			"Regularly clean decorations, filters, and other equipment in the tank to prevent the accumulation of algae and debris. Use a soft brush or sponge to gently scrub decorations, and rinse filter media in dechlorinated water to remove dirt and debris without harming beneficial bacteria.",
+	},
+];
+
+const fishLocations = ["Center Hill Lake", "Dale Hollow Lake", "Percy Priest Lake", "Chickamauga Lake", "Reelfoot Lake", "Norris Lake"];
+
+export const LocationsList = () => {
+	return fishLocations.map((lakes) => ({ ...lakes }));
 };
 
 export const FishList = () => {
